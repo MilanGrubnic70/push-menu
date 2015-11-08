f4f46c09eb49710b5bf16e0a15e9ac305b5a5474
Milan Grubnić <MilanG@aol.com>
2015-11-07 22:35:57 -0600

    debug

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var main = function  () {
-	$('.icon-menu').click(function  () {
-		$(.menu).animate({
-			left: 0px,
-			duration: 200
-		});
-		$(.body).animate({
-			left: 285px;
-			duration; 200
-		});
-	});
+var main = function() {
+    $('.icon-menu').click(function() {
+        $('.menu').animate({
+            left: '0px',
+            duration: 200
+        });
+        $('body').animate({
+            left: '285px',
+            duration: 200
+        });
+    });
 };
 
 $(document).ready(main);
